fe1782233f9110c55870f6d05f4134757a01998a
Michal Terranova <michalterranova@mail.com>
2019-08-21 19:27:43 -0400

    Setting up link to mysql

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+//create connection
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
+
+connection.connect((err)=>{
+    if(err) {throw err;}
+    console.log('MySQL now connected...');
+});
